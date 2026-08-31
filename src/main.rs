7cea9975e07c3dee176013f2755a2cbd64ef9a1b
Andreas Geyer <andreas@qbitone.de>
2026-08-31 20:11:33 +0200

    refactor: apply clippy settings

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ use axum::{Router, routing::get};
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+
+    let address = "0.0.0.0:3000";
+    let listener = tokio::net::TcpListener::bind(address)
+        .await
+        .unwrap_or_else(|_| panic!("Failed to bind to address: {address}"));
+    let _ = axum::serve(listener, app).await;
 }
