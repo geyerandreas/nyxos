5950f0a3e08dc624b1f9e01badf33426d0fd4347
Andreas Geyer <andreas@qbitone.de>
2026-08-31 20:18:37 +0200

    chore: add initial structured logging

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use axum::{Router, routing::get};
+use tracing::info;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
+
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
     let address = "0.0.0.0:3000";
     let listener = tokio::net::TcpListener::bind(address)
         .await
         .unwrap_or_else(|_| panic!("Failed to bind to address: {address}"));
+    info!("The server has been started on http://{address}");
     let _ = axum::serve(listener, app).await;
 }
