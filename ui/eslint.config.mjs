b649ccce1808cefa8cbff3479aa430229b70a876
Andreas Geyer <andreas@qbitone.de>
2026-09-22 17:42:21 +0200

    chore: format config files

--- ui/eslint.config.mjs
+++ ui/eslint.config.mjs
@@ -3,17 +3,14 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 import betterTailwindcss from 'eslint-plugin-better-tailwindcss'
 import { getDefaultAttributes } from 'eslint-plugin-better-tailwindcss/api/defaults'
 
-export default withNuxt(
-  betterTailwindcss.configs['correctness-error'],
-  {
-    settings: {
-      'better-tailwindcss': {
-        entryPoint: 'app/assets/css/main.css',
-        attributes: [
-          ...getDefaultAttributes(),
-          ['^v-bind:ui$', [{ match: 'objectValues' }]]
-        ]
-      }
+export default withNuxt(betterTailwindcss.configs['correctness-error'], {
+  settings: {
+    'better-tailwindcss': {
+      entryPoint: 'app/assets/css/main.css',
+      attributes: [
+        ...getDefaultAttributes(),
+        ['^v-bind:ui$', [{ match: 'objectValues' }]]
+      ]
     }
   }
-)
+})
